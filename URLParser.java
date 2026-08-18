33bbf14fed3b5d301ba66394b242ca71cd530afe
Allankamaum <akamau41@gmail.com>
2026-08-18 14:24:31 +0300

    Initial commit + URLParser document

---
+++ URLParser.java
@@ -0,0 +1,23 @@
+import java.net.URL;
+import java.net.URLDecoder;
+
+public class URLParser {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://api.techstore.com:8443/products/category/electronics?sort=price&limit=20#reviews");
+
+        System.out.println("Protocol: " + url.getProtocol());
+        System.out.println("Host: " + url.getHost());
+        System.out.println("Port: " + url.getPort());
+        System.out.println("Path: " + url.getPath());
+        System.out.println("Fragment: " + url.getRef());
+
+        String query = url.getQuery();
+        System.out.println("Query Parameters:");
+        for (String param : query.split("&")) {
+            String[] pair = param.split("=");
+            String key = URLDecoder.decode(pair[0], "UTF-8");
+            String value = URLDecoder.decode(pair[1], "UTF-8");
+            System.out.println("  " + key + " = " + value);
+        }
+    }
+}
